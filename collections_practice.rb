41dc0dfc9d182969dc4a7f7a92237103b8ce1f76
Steven Perfors <stevecanbuild@gmail.com>
2020-02-28 21:43:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,6 +10,7 @@ def sort_array_char_count(array)
   array.sort! {|a,b| a.length <=> b.length}
 end
 
-def swap_elements(array)
+def reverse_array(array)
   array[1], array[2] = array[2], array[1]
+  array
 end
